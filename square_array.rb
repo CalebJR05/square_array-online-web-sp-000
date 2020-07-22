460b35ceb5946cb09b7349faba5d7f886270dc9f
Caleb R <cjr4912@gmail.com>
2020-07-22 16:30:21 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,8 @@
 def square_array(array)
-  squared_numbers = array.each { |number| number ** 2 }
-  
+  squared_numbers = []
+  array.each do |number|
+  squared_numbers << number ** 2
+  end
   return squared_numbers
   
 end
