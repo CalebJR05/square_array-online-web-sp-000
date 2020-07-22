df2e3734e81cb75898ecee80547eec2a78b0b595
Caleb R <cjr4912@gmail.com>
2020-07-22 16:29:07 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,6 @@
 def square_array(array)
   squared_numbers = []
-  array.each do |number|
-  squared_numbers << number ** 2
-  end
+  squared_numbers = array.each { |number| number ** 2}
   return squared_numbers
   
 end
